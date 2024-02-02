82d7b13def0ed3783396c98e7907b8e61bf1ea5c
Beksultan078 <150646990+Beksultan078@users.noreply.github.com>
2024-02-02 11:22:24 +0600

    Create myfamily.py

---
+++ myfamily.py
@@ -0,0 +1,13 @@
+myfamily = ("mother", "father", "sister", "brother", "sister")
+
+# Print the tuple
+print(myfamily)
+
+# 1. Use type() to check the type of the object myfamily
+print("Type of myfamily:", type(myfamily))
+
+# 2. Access tuple items "sister" by using index numbers
+first_sister = myfamily[2]
+second_sister = myfamily[4]
+print("First sister:", first_sister)
+print("Second sister:", second_sister)
